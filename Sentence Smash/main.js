acd332cca40e54927e1c3bdd5b21cf758b73ebfb
omarsalem7 <omarsalem0721@gmail.com>
2022-03-15 09:14:11 +0200

    solve smash sentance problem

--- Sentence Smash/main.js
+++ Sentence Smash/main.js
@@ -1,3 +1,10 @@
-function smash (words) {
-    return words.join(' ')
- };
+function smash(words) {
+  if (!words.length) {
+    return '';
+  }
+  let res = '';
+  for (let i = 0; i < words.length; i++) {
+    i === 0 ? (res = res + words[i]) : (res = res + ' ' + words[i]);
+  }
+  return res;
+}
